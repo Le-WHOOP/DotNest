4b0a4e8c6a2186b55fbb6340104aa6e30bda0edd
Abigaëlle Panhelleux <100417910+LunaPak14@users.noreply.github.com>
2025-05-26 16:08:18 +0200

    [Rental] started rental's list

---
+++ DotNest/Controllers/RentalController.cs
@@ -0,0 +1,25 @@
+﻿using DotNest.Models;
+using DotNest.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace DotNest.Controllers
+{
+    public class RentalController : Controller
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public RentalController(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
